6c7a3ced20f33e824ecc2fb676d3bb9b07278c89
Matthew <mtwichan@gmail.com>
2022-06-25 16:45:54 -0400

    feat: test

--- test/echo.test.ts
+++ test/echo.test.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { Echo } from "../typechain";
-import {EchoJSON} from "../artifacts/contracts/Echo.sol/Echo.json";
+import EchoJSON from "../artifacts/contracts/Echo.sol/Echo.json";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Contract } from "ethers";
 dotenv.config();
@@ -12,16 +12,16 @@ describe("Echo Contract", () => {
   let echoContract: Contract;
   beforeEach(async () => {
     [deployer] = await ethers.getSigners();
-    const provider = ethers.getDefaultProvider('mumbai');
+    const provider = new ethers.providers.JsonRpcProvider(`https://rpc-mumbai.maticvigil.com/v1/${process.env.ALCHEMY_API_KEY}`);
     const privateKey = process.env.PRIVATE_KEY_MATIC || "";
-    const wallet = new ethers.Wallet(privateKey, provider);
-    const contractAddress = "0x377b233AFDA8669f33Cb4af7A8562e5d7f353A19";
-    echoContract = await new ethers.Contract(contractAddress, EchoJSON, provider);
+    const contractAddress = "0xB149b57dD9EF08A9728612dAE867c2e602480D21";
+    echoContract = await new ethers.Contract(contractAddress, EchoJSON.abi, provider);
   });
 
   it("Can log a message", async () => {
-    const tx = await echoContract.connect(deployer).logMessage("0x123", "hello world");
+    const tx = await echoContract.connect(deployer).logMessage("0xcd3b766ccdd6ae721141f452c550ca635964ce71", "hello world");
     const txReceipt = await tx.wait();
     console.log(txReceipt);
+    console.log(txReceipt.events);
   });
 });
