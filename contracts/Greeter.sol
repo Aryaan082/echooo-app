f06c3190c38f8b42099c68005873ac3ae36b4ba8
Matthew <mtwichan@gmail.com>
2022-06-25 12:23:20 -0400

    feat: change hardhat to typescript & add react frontend

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -3,10 +3,20 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract Echo {
-    
-    event LogMessage(address indexed reciever, string message);
-    event LogIdentity(address indexed communicationAddress, address indexed identityAddress);
+contract Greeter {
+    string private greeting;
 
-    
+    constructor(string memory _greeting) {
+        console.log("Deploying a Greeter with greeting:", _greeting);
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+        greeting = _greeting;
+    }
 }
