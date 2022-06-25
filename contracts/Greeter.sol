248ecc74c0a5a645efc6eb7b5d7bcbae0a7e3ffa
Aryaan <aryaan.ab.bhimani@gmail.com>
2022-06-25 12:03:44 -0400

    feat: init commit

---
+++ contracts/Greeter.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Echo {
+    
+    event LogMessage(address indexed reciever, string message);
+    event LogIdentity(address indexed communicationAddress, address indexed identityAddress);
+
+    
+}
