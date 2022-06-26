8f956cf817bc49564cfb569d6af9fbbfb67b5de2
Aryaan Bhimani <aryaan.ab.bhimani@gmail.com>
2022-06-26 01:43:32 -0400

    connect to smart contract

---
+++ frontend/src/interact.js
@@ -0,0 +1,14 @@
+// interact.js
+
+const API_KEY = process.env.API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+// Provider
+const alchemyProvider = new ethers.providers.AlchemyProvider(network="mumbai", API_KEY);
+
+// Signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// Contract
+const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
