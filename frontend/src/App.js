7d453227bbea8ad81c4445d0678aae643c59b328
Aryaan <aryaan.ab.bhimani@gmail.com>
2022-06-25 22:40:46 -0400

    Front end build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,116 +1,67 @@
-import {ethers} from "ethers";
-import Web3Modal from "web3modal";
-import {React, useState, useEffect } from 'react';
-import WalletConnect from "@walletconnect/web3-provider";
-// import MetaMask from "@metamask/web3-provider";
+import '@rainbow-me/rainbowkit/styles.css'
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+import logo from './assets/echooo.svg';
+import textBubble from './assets/text-bubble.svg';
+import matic from './assets/matic.svg';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism],
+  [
+    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
 export default function App() {
-    const [provider, setProvider] = useState();
-    const [account, setAccount] = useState();
-    const [error, setError] = useState("");
-    const [chainId, setChainId] = useState();
-    const [network, setNetwork] = useState();
-    const [message, setMessage] = useState("");
-    const [signedMessage, setSignedMessage] = useState("");
-    const [verified, setVerified] = useState();
-    
-    const web3Modal = new Web3Modal({
-        cacheProvider: false, // optional
-        walletconnect: {
-            package: WalletConnect, // required
-            options: {
-              infuraId: process.env.INFURA_KEY // required
-
-            }
-          },
-        network: "mumbai"
-      });
-
-    const connectWallet = async () => {
-      try {
-        const provider = await web3Modal.connect();
-        const library = new ethers.providers.Web3Provider(provider);
-        const accounts = await library.listAccounts();
-        const network = await library.getNetwork();
-        setProvider(provider);
-        if (accounts) setAccount(accounts[0]);
-        setChainId(network.chainId);
-      } catch (error) {
-        setError(error);
-      }
-    };
-  
-    const refreshState = () => {
-      setAccount();
-      setChainId();
-      setNetwork("");
-      setMessage("");
-      setVerified(undefined);
-    };
-  
-    const disconnect = async () => {
-      await web3Modal.clearCachedProvider();
-      refreshState();
-    };
-  
-    useEffect(() => {
-      if (web3Modal.cachedProvider) {
-        connectWallet();
-      }
-    }, []);
-  
-    useEffect(() => {
-      if (provider?.on) {
-        const handleAccountsChanged = (accounts) => {
-          console.log("accountsChanged", accounts);
-          if (accounts) setAccount(accounts[0]);
-        };
-  
-        const handleChainChanged = (_hexChainId) => {
-          setChainId(_hexChainId);
-        };
-  
-        const handleDisconnect = () => {
-          console.log("disconnect", error);
-          disconnect();
-        };
-  
-        provider.on("accountsChanged", handleAccountsChanged);
-        provider.on("chainChanged", handleChainChanged);
-        provider.on("disconnect", handleDisconnect);
-  
-        return () => {
-          if (provider.removeListener) {
-            provider.removeListener("accountsChanged", handleAccountsChanged);
-            provider.removeListener("chainChanged", handleChainChanged);
-            provider.removeListener("disconnect", handleDisconnect);
-          }
-        };
-      }
-    }, [provider]);
-  
-    return (
-      <div>
-        <div>
-            {!account ? (
-              <button onClick={connectWallet}>Connect Wallet</button>
-            ) : (
-              <button onClick={disconnect}>Disconnect</button>
-            )}
-          </div>
-          <div>
-              <div>{`Connection Status: `}</div>
-              {account ? (
-                <div>Logged In</div>
-              ) : (
-                <div>Logged Out</div>
-              )}
-            </div>
-  
-            <div label={account} placement="right">
-              <div>{`Account: ${account}`}</div>
-            </div>
-
+  return (
+    <div className='flex flex-row h-[100vh] bg-gradient-bg' style={{backgroundRepeat: "no-repeat"}}>
+      <div className='flex flex-col w-[25%] border-r-[2px] border-slate-300'>
+        
       </div>
-    );
-  }
-  
+      <div className='flex flex-row items-center justify-between px-[20px] py-[40px] h-[100px]'>
+        <img className='h-[30px]' src={logo}>
+        </img>
+        <div className='flex flex-row gap-4'>
+          <div className='flex flex-row gap-[10px] bg-sky-100 px-[15px] py-[10px] rounded-md text-black font-bold'>
+            <img src={matic}>
+            </img>
+            Polygon
+          </div>
+          <WagmiConfig client={wagmiClient}>
+            <RainbowKitProvider chains={chains}>
+              <ConnectButton />
+            </RainbowKitProvider>
+          </WagmiConfig>
+          <div className='flex flex-row gap-[10px] bg-gradient-to-r from-cyan-500 to-pink-600 px-[15px] py-[10px] rounded-3xl text-white'>
+            Start new chat
+            <img src={textBubble}></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-bg': "url('/src/assets/gradient-bg.svg')",
+      }
+    },
+  },
+  plugins: [],
+}
