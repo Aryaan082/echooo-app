18455553ce2e7be2081429f4196a6ef7b2a16606
Aryaan Bhimani <aryaan.ab.bhimani@gmail.com>
2022-08-01 01:26:06 -0400

    Fix communication keys bug

--- frontend/src/contracts/ContractInstances.js
+++ frontend/src/contracts/ContractInstances.js
@@ -1,18 +1,18 @@
-import { useContract, useSigner } from "wagmi";
+import { useContract, useNetwork, useSigner } from "wagmi";
 
 import EchoJSON from "./Echo.sol/Echo.json";
 
+const contractEchoAddress = {
+  43113: "0x79DD6a9aF59dE8911E5Bd83835E960010Ff6887A",
+  80001: "0x21e29E3038AeCC76173103A5cb9711Ced1D23C01",
+};
+
 export default async function ContractInstances() {
   const { data: signer } = useSigner();
-  const chainId = await parseInt(window.ethereum.networkVersion);
-
-  const contractEchoAddress = {
-    43113: "0x79DD6a9aF59dE8911E5Bd83835E960010Ff6887A",
-    80001: "0x21e29E3038AeCC76173103A5cb9711Ced1D23C01",
-  };
+  const { chain } = useNetwork();
 
   const contractEcho = useContract({
-    addressOrName: contractEchoAddress[chainId],
+    addressOrName: contractEchoAddress[chain.id],
     contractInterface: EchoJSON.abi,
     signerOrProvider: signer,
   });
